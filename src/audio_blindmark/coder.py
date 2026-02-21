761e3208fa7a92573e8fcc1f5e2e678d04d34aa7
NightSpaceC <NightSpaceC@outlook.com>
2026-02-21 14:45:10 +0800

    prof(framework): use ChaCha20 instead of AES

--- src/audio_blindmark/coder.py
+++ src/audio_blindmark/coder.py
@@ -2,7 +2,7 @@ import base64
 from typing import Optional
 
 from blake3 import blake3
-from Crypto.Cipher import AES
+from Crypto.Cipher import ChaCha20
 from Crypto.Hash import SHA512
 from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Random import get_random_bytes
@@ -21,17 +21,17 @@ class ImpossibleOutputLengthError(Exception):
     pass
 
 class Encoder:
-    def __init__(self, password: bytes, ecc_length: int, digest_length = 4, salt_length = 4, nonce_length = 4) -> None:
+    def __init__(self, password: bytes, ecc_length: int, digest_length = 8, salt_length = 8) -> None:
         self.password = password
         self.ecc_length = ecc_length
         self.digest_length = digest_length
         self.salt_length = salt_length
-        self.nonce_length = nonce_length
+        self.nonce_length = 8  # The requirement of ChaCha20 can not be modified
 
         self.rsc = RSCodec(ecc_length)
 
-        key_over = PBKDF2(base64.b64encode(password).decode(), b'', 24, DERIVATION_EPOCH, hmac_hash_module = DERIVATION_HASH)
-        cipher_over = AES.new(key_over, AES.MODE_CTR, nonce = b'')
+        key_over = PBKDF2(base64.b64encode(password).decode(), b'', 32, DERIVATION_EPOCH, hmac_hash_module = DERIVATION_HASH)
+        cipher_over = ChaCha20.new(key = key_over, nonce = b'\x00' * 8)
         self.key_stream = cipher_over.encrypt(b'\x00' * MAX_DATA_LENGTH)
 
         self.key_cache: Optional[tuple[bytes, bytes]] = None
@@ -46,11 +46,11 @@ class Encoder:
             salt = self.key_cache[0]
         else:
             salt = get_random_bytes(self.salt_length)
-            self.key_cache = (salt, PBKDF2(base64.b64encode(self.password).decode(), salt, 24, DERIVATION_EPOCH, hmac_hash_module = DERIVATION_HASH))
+            self.key_cache = (salt, PBKDF2(base64.b64encode(self.password).decode(), salt, 32, DERIVATION_EPOCH, hmac_hash_module = DERIVATION_HASH))
 
         nonce = get_random_bytes(self.nonce_length)
 
-        cipher = AES.new(self.key_cache[1], AES.MODE_CTR, nonce = nonce)
+        cipher = ChaCha20.new(key = self.key_cache[1], nonce = nonce)
         cipher_text = cipher.encrypt(seq.to_bytes(4, 'little') + data)
         cipher_text_with_meta = bytes(random_tools.encode(salt)) + nonce + cipher_text
         text_with_ecc: bytes = bytes(iter(self.rsc.encode(iter(blake3(cipher_text_with_meta).digest(self.digest_length) + cipher_text_with_meta))))
@@ -62,17 +62,17 @@ class DecodeError(Exception):
     pass
 
 class Decoder:
-    def __init__(self, password: bytes, ecc_length: int, digest_length = 4, salt_length = 4, nonce_length = 4) -> None:
+    def __init__(self, password: bytes, ecc_length: int, digest_length = 8, salt_length = 8) -> None:
         self.password = password
         self.ecc_length = ecc_length
         self.digest_length = digest_length
         self.salt_length = salt_length
-        self.nonce_length = nonce_length
+        self.nonce_length = 8  # The requirement of ChaCha20 can not be modified
 
         self.rsc = RSCodec(ecc_length)
 
-        key_over = PBKDF2(base64.b64encode(password).decode(), b'', 24, DERIVATION_EPOCH, hmac_hash_module = DERIVATION_HASH)
-        cipher_over = AES.new(key_over, AES.MODE_CTR, nonce = b'')
+        key_over = PBKDF2(base64.b64encode(password).decode(), b'', 32, DERIVATION_EPOCH, hmac_hash_module = DERIVATION_HASH)
+        cipher_over = ChaCha20.new(key = key_over, nonce = b'\x00' * 8)
         self.key_stream = cipher_over.encrypt(b'\x00' * MAX_DATA_LENGTH)
 
         self.key_cache: dict[bytes, bytes] = {}
@@ -105,9 +105,9 @@ class Decoder:
         if salt in self.key_cache:
             key = self.key_cache[salt]
         else:
-            self.key_cache[salt] = key = PBKDF2(base64.b64encode(self.password).decode(), salt, 24, DERIVATION_EPOCH, hmac_hash_module = DERIVATION_HASH)
+            self.key_cache[salt] = key = PBKDF2(base64.b64encode(self.password).decode(), salt, 32, DERIVATION_EPOCH, hmac_hash_module = DERIVATION_HASH)
 
-        cipher = AES.new(key, AES.MODE_CTR, nonce = nonce)
+        cipher = ChaCha20.new(key = key, nonce = nonce)
         text = cipher.decrypt(cipher_text)
         try:
             return (int.from_bytes(text[:4], 'little'), text[4:])
